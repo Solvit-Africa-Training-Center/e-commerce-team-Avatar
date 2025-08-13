b59b02d6d280a0e68c5f807fdbeff8ad95104aae
jeid12 <niyokwizerajd123@gmail.com>
2025-08-13 12:32:39 +0200

    update

--- product/models.py
+++ product/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-# products/models.py
+
 class Category(models.Model):
     name = models.CharField(max_length=255, unique = True)
     description = models.TextField(blank=True)
